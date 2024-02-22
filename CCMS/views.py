d8e8362839c29156a3ca9d764d9dd938a84ad3b2
Joshua Laude <Jp.laude@mseuf.edu.ph>
2024-02-22 13:51:11 +0800

    Successfully added views.py and 3 functions named Vision, Mission, Objectives. Also successfully finished using those 3 functions in views.py to have a string query of url in the urls.py. Note: The string query of the url is not Multiple(e.g 127.0.0.1:8000/CCMS/Vision) it is only (e.g 127.0.0.1:8000/Vision)

---
+++ CCMS/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def Mission(request):
+    msg = '<h1>CCMS Mission</h1> \
+    <p>The College of Computer Studies shall produce \
+    technically competent Information Technology professionals adequately \
+    prepared in the practice of their profession supportive of \
+        national development goals and standards of global excellence.</p>'
+    return HttpResponse(msg);
+
+def Vision(request):
+    msg = '<h1>CCMS Vision</h1> \
+    <p>The College of Computer Studies shall be a center of excellence in Information \
+    Technology education.</p>'
+    return HttpResponse(msg);
+
+def Objectives(request):
+    msg = '<h1>CCMS Objectives</h1> \
+    <p>After graduation, alumni of MSEUF-CCS programs shall:<br><br> \
+    1. Be employed and demonstrate professionalism, competence and passion in solving \
+    contemporary computing problems by developing or utilizing innovative IT solutions.<br><br> \
+    2. Embark in lifelong learning or research to attune to the continuous innovation in the IT \
+    industry in order to adapt to the changing demands of the global market; and <br><br> \
+    3. Exhibit leadership and teamwork, and commitment to their respective local or global organizations.</p>'
+    return HttpResponse(msg);
